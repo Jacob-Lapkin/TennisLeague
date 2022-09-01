0a7b89e0f842897c213be306a20d11be7d245bbc
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-09-01 12:22:46 -0700

    fix nav

--- news.py
+++ news.py
@@ -1,15 +1,31 @@
 from newsapi import NewsApiClient
-import datetime 
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
 news_api = os.getenv("news_api_key")
 # Init
+if datetime.now().month != 1:
+    year = datetime.now().year
+    month = datetime.now().month - 1
+if datetime.now().month == 1:
+    year = datetime.now().year - 1
+    month = 12
+day = datetime.now().day
+if day < 10 and month < 10:
+    from_date = f'{year}-0{month}-0{day}'
+elif day < 10:
+    from_date = f'{year}-{month}-0{day}'
+elif month < 10:
+    from_date = f'{year}-0{month}-{day}'
+else: 
+        from_date = f'{year}-{month}-{day}'
+
 newsapi = NewsApiClient(api_key=news_api)
-all_articles = newsapi.get_everything(q='US Open',
-                                      from_param='2022-08-01',
+all_articles = newsapi.get_everything(q='Kyrgios',
+                                      from_param=from_date,
                                       language='en',
                                       sort_by='relevancy',
                                       page=1)
@@ -19,4 +35,5 @@ def get_kyrgios_news():
     # /v2/everything
     only_articles = all_articles['articles']
     return only_articles
+
 
--- views/home.py
+++ views/home.py
@@ -5,5 +5,5 @@ home = Blueprint("home", __name__, url_prefix='', template_folder='templates')
 
 @home.route('/home', methods=["GET", "POST"])
 def home_page():
-    kyrgios_news = get_kyrgios_news
+    kyrgios_news = get_kyrgios_news()
     return render_template('home.html', kyrgios_news=kyrgios_news)
