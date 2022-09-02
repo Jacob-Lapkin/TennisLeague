b8798423eac3d90f4debca5d586f88cd9dff1e22
Jacob Lapkin <65730020+Jacob-Lapkin@users.noreply.github.com>
2022-09-02 15:08:10 -0700

    search league

--- views/leagues.py
+++ views/leagues.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, Blueprint, flash, session, redirect
 from database import mongo
 
-leagues = Blueprint("leages", __name__, url_prefix='')
+leagues = Blueprint("leagues", __name__, url_prefix='')
 
 
 @leagues.route('/leagues', methods=['GET', 'POST'])
